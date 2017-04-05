9f3d70276e2cff7938e26a9935b98a02a08ec6e7
Karl Redgate <Karl.Redgate@gmail.com>
2017-04-05 07:56:18 -0400

    add spawn

--- experimental/erlang/tcpserv.erl
+++ experimental/erlang/tcpserv.erl
@@ -5,15 +5,24 @@
 start() ->
     Options = [binary, {reuseaddr, true}, {active,true}],
     {ok, Listener} = gen_tcp:listen( 8080, Options ),
-    connection( Listener ) .
+    acceptor(Listener) .
 
-connection(Listener) ->
+acceptor(Listener) ->
+    % {Child} = spawn( tcpserv, connection, [Listener, self()] ),
+    % spawn( tcpserv, connection, [Listener, self()] ),
+    Parent = self(),
+    spawn( fun () -> connection(Listener, Parent) end ),
+    receive
+    {accepted} -> io:format("child accepted~n"), acceptor(Listener)
+    end .
+
+connection(Listener, Parent) ->
     {ok, Socket} = gen_tcp:accept(Listener),
-    inet:setopts( Socket, [binary, {packet,0}, {nodelay,true}, {active,true}] ),
-    % spawn( fun () -> loop(Socket) end ),
-    loop(Socket),
     io:format( "got connection~n" ),
-    connection(Listener) .
+    inet:setopts( Socket, [binary, {packet,0}, {nodelay,true}, {active,true}] ),
+    Parent ! {accepted},
+    % spawn( fun () -> loop(Socket) end ),
+    loop(Socket) .
 
 loop(Socket) ->
     io:format( "receive loop~n" ),
