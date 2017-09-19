b97110ad3ae4d75d00440e9c16faf7c16a6f51d2
Karl Redgate <Karl.Redgate@gmail.com>
2017-09-19 16:53:27 -0400

    some multitask testing

--- experimental/grunt/Gruntfile.js
+++ experimental/grunt/Gruntfile.js
@@ -1,9 +1,19 @@
 
 var config = {
+    // top level keys are "tasks"
+    multitask: {
+        // these second level keys are subtasks in a multitask
+        // How does it differentiate between subtatsks and options?
+        subtarget: {
+            file: "example.txt"
+        }
+    },
+    // JSHint is leniant JSLint
     jshint: {
         files: ['Gruntfile.js', 'src/**/*.js']
     },
-    watch: {},
+    watch: {
+    },
     connect: {
         server: {
             options: {
@@ -14,6 +24,23 @@ var config = {
         }
     }
 };
+
+// Mock target/task
+//
+// "this" is bound to the task object - I think
+// The config for grunt is stored in grunt.config, but
+// this is bound in the module exports function closure.
+// How do we get access in a more global function?
+
+function multitask( arg ) {
+    console.log( this );
+
+    // grunt is bound in the current env of the exports function
+    // grunt.config.requires( 'file' );
+    // this.requires( 'file' ); -- not work
+    this.requiresConfig( 'multitask.subtarget.file' );
+    console.log( "TARGET: " + arg );
+}
 
 function help() {
     console.log( "No tasks defined yet." );
@@ -32,6 +59,7 @@ module.exports = function (grunt) {
    grunt.loadNpmTasks( 'grunt-contrib-watch' );
    grunt.loadNpmTasks( 'grunt-contrib-connect' );
 
+   grunt.registerTask( 'multitask', multitask );
    grunt.registerTask( 'help', help );
    grunt.registerTask( 'nothing', nothing );
    grunt.registerTask( 'server', ['connect:server:keepalive'] );
@@ -39,4 +67,15 @@ module.exports = function (grunt) {
    grunt.registerTask('default', ['help']);
 };
 
+// Task Object:
+// { nameArgs: 'multitask:subtarget',
+//   name: 'multitask',
+//   args: [ 'subtarget' ],
+//   flags: { subtarget: true },
+//   async: [Function],
+//   errorCount: [Getter],
+//   requires: [Function: bound ],
+//   requiresConfig: [Function],
+//   options: [Function] }
+//
 // vim:autoindent expandtab sw=4
