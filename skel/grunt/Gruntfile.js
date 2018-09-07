ee370f00e119d92b64e90e20687e29c2157b2c40
Karl Redgate <Karl.Redgate@gmail.com>
2018-09-07 10:22:40 -0400

    save grunt skel

---
+++ skel/grunt/Gruntfile.js
@@ -0,0 +1,26 @@
+function help() {
+    console.log( "No tasks defined yet." );
+}
+
+module.exports = function(grunt) {
+  grunt.registerTask( 'help', help );
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['jshint']);
+};
