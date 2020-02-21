9be5ede7707f2098b7f18a3e9fa09cf7eedaf4b6
Karl Redgate <Karl.Redgate@gmail.com>
2020-02-21 18:09:35 -0500

    add base files

---
+++ skel/java/Command.java
@@ -0,0 +1,11 @@
+
+public class Command {
+
+    public static void
+    main( String args[] ) {
+        System.out.println( "parse the command line" );
+    }
+
+}
+
+/* vim: set autoindent expandtab sw=4 : */
