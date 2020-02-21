1853d4135765072752547c831d9c3435eaaf6746
Karl Redgate <Karl.Redgate@gmail.com>
2020-02-21 18:23:54 -0500

    add more

--- skel/java/Command.java
+++ skel/java/Command.java
@@ -3,7 +3,13 @@ public class Command {
 
     public static void
     main( String args[] ) {
-        System.out.println( "parse the command line" );
+        // for each arg?
+        String arg = args[0];
+        System.out.println( "parse the command line " + arg );
+        String segments[] = arg.split("/");
+        for ( String segment : segments ) {
+            System.out.println( "Segment: " + segment );
+        }
     }
 
 }
