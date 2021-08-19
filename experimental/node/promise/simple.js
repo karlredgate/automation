8aef7f947ccda7267e9b7fbe61038539d10b1781
Karl Redgate <kredgate@bostondynamics.com>
2021-08-19 13:21:25 -0400

    example promise

---
+++ experimental/node/promise/simple.js
@@ -0,0 +1,40 @@
+
+var Defer = function () {
+    this.promise = new Promise();
+};
+Defer.prototype = {
+    promise = null
+};
+
+Defer.prototype.resolve = function (data) {
+    var handler = function (callback) {
+	var wrapper = function () { callback(data); };
+	setTimeout( wrapper, 0 );
+    };
+    this.promise.success.forEach( handler );
+};
+Defer.prototype.reject = function (error) {
+    var handler = function (callback) {
+	var wrapper = function () { callback(error); };
+	setTimeout( wrapper, 0 );
+    };
+    this.promise.failure.forEach( handler );
+};
+
+var Promise = function () {
+    this.success = [];
+    this.failure = [];
+};
+
+Promise.prototype = {
+    success = null,
+    failure = null
+};
+
+Promise.prototype.then = function (good, err) {
+    var defer = new Defer();
+    success.push( good );
+    if ( err ) {
+	failure.push( err );
+    }
+};
