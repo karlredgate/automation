85dd5c98c4c67ff9e1ad5a7eab45a485efa8cf50
Karl Redgate <kredgate@bostondynamics.com>
2022-08-29 15:11:03 -0400

    more ex

--- experimental/go/caste.go
+++ experimental/go/caste.go
@@ -3,6 +3,10 @@ package main
 
 import "fmt"
 
+type BB struct {
+    s string
+}
+
 func main() {
     var i interface{}
     i = "a string"
@@ -11,8 +15,13 @@ func main() {
     i = 2022
     i = 3.145
     // this syntax only works for an interface
+    //   and is called a "type assertion" - does this interface contain this type?
     r := i.(float64)
     fmt.Println( "val",r )
+
+    var i2 interface{} = BB{"some string"}
+    fmt.Println( "i2",i2.(BB) )
     a := []byte( `hello` )
+    // this is a typecast
     fmt.Println( "a",a,"stringVal",string(a) )
 }
