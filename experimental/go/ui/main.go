37bc0ba0e95ce1f355cfbbb4b512099bdbc8a193
Karl Redgate <karlredgate@owsley.local>
2023-05-07 15:22:26 -0400

    updates

--- experimental/go/ui/main.go
+++ experimental/go/ui/main.go
@@ -19,7 +19,12 @@ func newTextPrim( text string ) (tview.Primitive) {
     return widget
 }
 
-var names [32]*tview.TableCell
+var focus     int = 0
+var widgets   [3]tview.Primitive
+
+var names     [32]*tview.TableCell
+var addresses [32]*tview.TableCell
+var states    [32]*tview.TableCell
 
 var content *tview.TextView
 
@@ -28,49 +33,21 @@ func update( app *tview.Application ) {
     for {
         time.Sleep( 1 * time.Second )
 	i += 3
-	name := fmt.Sprintf( "STUFFandnonsense.%d object tyupe etc\n", i )
+	name := fmt.Sprintf( "beta-%d", i )
 	names[20].SetText(name)
-	fmt.Fprintf( content, name )
+	line := fmt.Sprintf( "STUFFandnonsense.%d object tyupe etc\n", i )
+	fmt.Fprintf( content, line )
     }
 }
 
-func main() {
-    grid := tview.NewGrid()
-    grid.SetRows( 1, 0, 10 ).SetColumns( 37, 0 ).SetBorders( true )
-
-    content = tview.NewTextView()
-    content.SetDynamicColors( true )
-
-    tbl := tview.NewTable()
-    tbl.SetFixed(1,1)
-
-    tbl.SetCell( 0, 0, tview.NewTableCell("Robot") )
-    tbl.SetCell( 0, 1, tview.NewTableCell("Address") )
-    tbl.SetCell( 0, 2, tview.NewTableCell("State") )
-
-    names[1] = tview.NewTableCell( "beta46" )
-    names[1].SetTextColor( tcell.ColorGreen )
-    tbl.SetCell( 1, 0, names[1] )
-
-    cell_2 := tview.NewTableCell( "10.10.60.60" )
-    tbl.SetCell( 1, 1, cell_2 )
-    cell_3 := tview.NewTableCell( "WAITING" )
-    tbl.SetCell( 1, 2, cell_3 )
-
-    names[2] = tview.NewTableCell( "[green]corgi86" )
-    tbl.SetCell( 2, 0, names[2] )
-
-    cell_5 := tview.NewTableCell( "LIVE" )
-    tbl.SetCell( 2, 2, cell_5 )
-
-    names[20] = tview.NewTableCell( "beta99" )
-    tbl.SetCell( 20, 0, names[20] )
-
-    app := tview.NewApplication()
-    content.SetChangedFunc( func () { app.Draw() } )
-
+func addShortcuts(app *tview.Application) (*tview.Application) {
     handler := func (event *tcell.EventKey) (*tcell.EventKey) {
 	switch event.Key() {
+	case tcell.KeyTab:
+	    focus = (focus + 1) % 3
+	    app.SetFocus( widgets[focus] )
+	    app.Sync()
+	    return nil
 	case tcell.KeyCtrlL:
 	    app.Sync()
 	    return nil
@@ -83,15 +60,76 @@ func main() {
 	    // have tab shift focus
 	    }
 	}
-	fmt.Println( "KEY", event.Key() )
+	// fmt.Println( "KEY", event.Key() )
 	return event
     }
     app.SetInputCapture( handler )
+    return app
+}
 
-    menu := tview.NewList()
-    menu.AddItem( "01234567-0123-0123-0123-012345678900", "ObjectType", 0, nil )
-    menu.AddItem( "12345670-0123-0123-0123-012345678900", "ObjectType", 0, nil )
-    // menu.AddItem( "Quit", "Press to exit", 'q', func () {app.Stop()} )
+func createLogWidget(app *tview.Application) (*tview.TextView, error) {
+    widget := tview.NewTextView()
+    widget.SetDynamicColors( true )
+    widget.SetChangedFunc( func () { app.Draw() } )
+    return widget,nil
+}
+
+func createCells(table *tview.Table, column int, list []*tview.TableCell) {
+    for i := 0; i < 32; i++ {
+	list[i] = tview.NewTableCell("")
+        list[i].SetTextColor( tcell.ColorGreen )
+        table.SetCell( i+1, column, list[i] )
+    }
+}
+
+func createRobotTableWidget(app *tview.Application) (*tview.Table, error) {
+    widget := tview.NewTable()
+    widget.SetFixed(1,1)
+
+    widget.SetCell( 0, 0, tview.NewTableCell("Idx") )
+    widget.SetCell( 0, 1, tview.NewTableCell("Robot") )
+    widget.SetCell( 0, 2, tview.NewTableCell("Address") )
+    widget.SetCell( 0, 3, tview.NewTableCell("State") )
+
+    for i := 0; i < 32; i++ {
+	cell := tview.NewTableCell( string(i) )
+        cell.SetTextColor( tcell.ColorGreen )
+        widget.SetCell( i, 0, cell )
+    }
+    createCells( widget, 1, names[:] )
+    createCells( widget, 2, addresses[:] )
+    createCells( widget, 3, states[:] )
+
+    return widget,nil
+}
+
+// menu.AddItem( "Quit", "Press to exit", 'q', func () {app.Stop()} )
+func createObjListWidget(app *tview.Application) (*tview.List, error) {
+    widget := tview.NewList()
+    widget.AddItem( "01234567-0123-0123-0123-012345678900", "ObjectType", 0, nil )
+    widget.AddItem( "12345670-0123-0123-0123-012345678900", "ObjectType", 0, nil )
+    widget.AddItem( "23456701-0123-0123-0123-012345678900", "ObjectType", 0, nil )
+    return widget,nil
+}
+
+func createApp() (*tview.Application,error) {
+    var err error
+
+    app := tview.NewApplication()
+    grid := tview.NewGrid()
+    grid.SetRows( 1, 0, 10 ).SetColumns( 37, 0 ).SetBorders( true )
+    app.SetRoot( grid, true ).EnableMouse( true )
+
+    content,err = createLogWidget( app )
+    if err != nil { return nil,err }
+    widgets[0] = content
+    addShortcuts( app )
+    tbl,err := createRobotTableWidget( app  )
+    if err != nil { return nil,err }
+    widgets[1] = tbl
+    menu,err := createObjListWidget( app )
+    if err != nil { return nil,err }
+    widgets[2] = menu
 
     // row,col,rspan,cspan,minheight,minwidth,focus
     grid.AddItem( newTextPrim("Protostore Status"), 0, 0, 1, 2, 0, 0, false)
@@ -100,10 +138,22 @@ func main() {
     grid.AddItem( content, 2, 0, 1, 2, 0, 0, false)
     fmt.Fprint( content, "[green]green[white] more" )
 
-    app.SetRoot( grid, true ).EnableMouse( true )
+    app.SetFocus( tbl )
+
+    return app,nil
+}
+
+func main() {
+    app,err := createApp()
+    if err != nil { panic(err) }
+
+    names[1].SetText( "beta46" )
+    addresses[1].SetText( "10.10.60.60" )
+    states[2].SetText( "WAITING" )
+    names[2].SetText( "corgi86" )
+    names[20].SetText( "beta99" )
+
     go update( app )
-    err := app.Run()
-    if err != nil {
-	panic(err)
-    }
+    err = app.Run()
+    if err != nil { panic(err) }
 }
